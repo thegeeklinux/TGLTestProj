a9a04a24524d91f5ca793b12a76f4e5f5034e752
root <root@f5b539b69030>
2016-07-26 12:07:17 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "TGLTestProj",
+  "homePage" : "Main",
+  "name" : "TGLTestProj",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
