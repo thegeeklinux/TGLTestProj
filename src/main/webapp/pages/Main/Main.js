eb3ef2c2d483966147166733e52cd4cc8a645670
Raju Gupta <raju.gupta@cloudjee.com>
2016-07-26 12:19:44 +0000

    New files added - [] Files modified - [Main.variables.json,pages-config.json,Main.html,Main.js,header.html,Main.css] Refactored files - [] Fixed bugs - [] Removed files - []

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -1,13 +1,18 @@
 Application.$controller("MainPageController", ["$scope", function ($scope) {
     "use strict";
 
-    /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function () {
+    /* perform any action on the variables within this block(on-page-load) */
+    $scope.onPageVariablesReady = function () {
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
          * $scope.Variables.loggedInUser.getData()
-         * 
+         */
+    };
+
+    /* perform any action on widgets within this block */
+    $scope.onPageReady = function () {
+        /*
          * widgets can be accessed through '$scope.Widgets' property here
          * e.g. to get value of text widget named 'username' use following script
          * '$scope.Widgets.username.datavalue'
@@ -15,3 +20,4 @@ Application.$controller("MainPageController", ["$scope", function ($scope) {
     };
 
 }]);
+
